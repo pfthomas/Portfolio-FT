7be00add2e881b60eb38d1ad83e2427162a028ac
pftho <pf.thomas97@gmail.com>
2022-12-09 17:31:22 -0500

    fixing routes again

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Header/>
           <Routes>
-            <Route path="/" 
+            <Route path="/Portfolio-FT/" 
             element={<Home/>}>
             </Route>
 
